1de7fd92d0d378b6295466b49c959849567267db
Naotoshi Yoshida <ney2@humboldt.edu>
2023-11-16 00:50:11 -0800

    copy_file project testing

---
+++ sample.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import shutil 
+
+# read data from csv
+data = pd.read_csv("testbook.csv") 
+
+# set file_types to read for specific values
+file_types = ['Deeds', 'PCORS', 'Affidavit']
+
+# filter the rows where the column 'Type' is in file_types 
+filtered_data = data[data['Type'].isin(file_types)]
+
+# print table of filtered rows 
+print(filtered_data)
+
+# create a variable with the file path of the folder containing the copies 
+copy_folder = r"C:\Users\nyoshida\Documents\python_code\copy_zone"
+
+# iterate through the rows in the filtered_data
+for index, row in filtered_data.iterrows(): 
+    # store the names of the filtered files into file_name by using the column name 
+    file_name = row['Name']
+    # check statement for copying the filtered files into the new folder 
+    try:
+        shutil.copy(file_name, copy_folder)
+        print(f'Successfully copied {file_name} to {copy_folder}')
+    except Exception as e:
+        print(f'Failed to copy {file_name} to {copy_folder}. Error: {e}')
