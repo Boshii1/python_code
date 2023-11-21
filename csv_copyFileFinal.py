4ea7250d9b5b2571f01d3ab10941157a400a3dd2
Boshii1 <89554589+Boshii1@users.noreply.github.com>
2023-11-20 16:55:49 -0800

    Add files via upload

--- csv_copyFileFinal.py
+++ csv_copyFileFinal.py
@@ -16,24 +16,24 @@ curr_date = now.strftime('%m-%d-%Y')
 print('Current date is : ' + curr_date)
 
 # specify the date, month and year and store them into separate variables 
-curr_year = now.year
+curr_year = str(now.year)
 print(curr_year)
-curr_month = now.month
+curr_month = str(now.month)
 print(curr_month)
-curr_day = now.day
+curr_day = str(now.day)
 print(curr_day)
 
 # create the directory path for the csv files
-csv_dirPath = os.path.join(r"C:\Users\nyoshida\Documents\python_code\")
+csv_dirPath = os.path.join(r'C:\Users\nyoshida\Documents\python_code') 
 print(csv_dirPath)
 
 # create the filename of the csv using the curr_date variable 
 # csv_fileName = curr_date + '.csv' 
 csv_fileNameTest = 'testbook.csv'
-print (csv_fileName)
+print (csv_fileNameTest)
 
 # create the filepath by using os.path.join to create the path from csv_dirPath and csv_fileName
-csv_filePath = os.path.join(csv_dirPath, csv_fileName)
+csv_filePath = os.path.join(csv_dirPath, csv_fileNameTest)
 print(csv_filePath)
 
 # to read from the csv we need to use the pd.read_csv method 
@@ -41,31 +41,37 @@ print(csv_filePath)
 # create a check to read data from csv 
 try: 
     data = pd.read_csv(csv_filePath)
-    print("File read succesfully!")
+    print('File read succesfully!')
 except FileNotFoundError: 
-    print(f"No file found at {csv_filePath}")
+    print(f'No file found at {csv_filePath}')
 except pd.errors.ParserError:
-    print(f"Could not parse the file at {csv_filePath}")
+    print(f'Could not parse the file at {csv_filePath}')
 except Exception as e: 
-    print(f"An unexpected error occurred: {e}")
+    print(f'An unexpected error occurred: {e}')
 
 # create a variable with all the specific document types to filter 
-file_types = [r"\bAF", "AG", r"\bCD", "CR09", "CR10", r"\bDE", r"\bFS", "GR01", "GR11",
-              "LE", "LE01", r"\bMI", r"\bNT", "TE", "TL", "TL06"]
+file_types = ['AF01', 'AF02', 'AF03', 'AF04', 'AF05', 'AF06', 'AF07', 'AFF', 'AG', 
+              'CD01', 'CD02', 'CD04', 'CD05', 'CD08', 'CR09', 'CR10', 'DE', 'DE03',
+              'DE06', 'DE07', 'DE08', 'DE09', 'FS', 'FS01', 'FS02', 'FS03', 'FS04',
+              'FS05', 'FS07', 'GR01', 'GR11', 'LE', 'LE01', 'MI', 'MI03', 'MI14',
+              'MI16', 'NT', 'NT01', 'NT02', 'NT06', 'NT09', 'NT11', 'NT21', 'TE',
+              'TL', 'TL06']
 
 # create a variable for the filtered document types
 filtered_data = data[data['Document' + ' ' + 'Code'].isin(file_types)]
 print(filtered_data)
 
 # create a variable with the file path of the folder containing the copies
-copy_folder = r"C:\Users\nyoshida\Documents\python_code\copy_zone"
+copy_folder = r'C:\Users\nyoshida\Documents\python_code\copy_zone'
 
 # iterate through the rows in the filtered_data dataframe 
 for index, row in filtered_data.iterrows(): 
-    # store the names of the filtered files into tiff_fileName by using the 'Document Code' column
-    tiff_fileName = row["Instrument Number"]
+    
+    # store the names of the file while iterating through the rows while the path is set 
+    source_tiffFile = os.path.join(r"C:\Users\nyoshida\Documents\python_code",curr_year,curr_month,curr_day,row['Instrument Number'] + ".txt") 
+    print(source_tiffFile)
     try:
-        shutil.copy(file_name, copy_folder)
-        print(f'Successfully copied {tiff_fileName} to {copy_folder}')
+        shutil.copy(source_tiffFile, copy_folder)
+        print(f'Successfully copied {source_tiffFile} to {copy_folder}')
     except Exception as e:
-        print(f'Failed to copy {tiff_fileName} to {copy_folder}. Error: {e}')
+        print(f'Failed to copy {source_tiffFile} to {copy_folder}. Error: {e}')
